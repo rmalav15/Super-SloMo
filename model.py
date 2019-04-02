29ec4670a089a1aabbb7a546fcae33bfec7da81a
Ram <rmalav15@gmail.com>
2019-04-02 14:24:35 +0530

    unet added

--- model.py
+++ model.py
@@ -5,8 +5,36 @@ from __future__ import print_function
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import keras
+from utils import vgg_19
+
+# Loss Helper Functions
+def reconstruction_loss(Ipred, Iref, scope="reconstruction_loss"):
+    return None
 
 
+def VGG19_slim(input, type, reuse):
+    # Define the feature to extract according to the type of perceptual
+    if type == 'VGG54':
+        target_layer = 'vgg_19/conv5/conv5_4'
+    elif type == 'VGG22':
+        target_layer = 'vgg_19/conv2/conv2_2'
+    else:
+        raise NotImplementedError('Unknown perceptual type')
+    _, output = vgg_19(input, reuse=reuse)
+    output = output[target_layer]
+
+    return output
+
+
+def perceptual_loss(Ipred, Iref, reuse, layers="VGG54"):
+    return None
+
+
+def back_wrap_bileanr(Image, Flow, scope="back_wrap_bileanr"):
+    return None
+
+
+# Model Helper Functions
 def conv2d(batch_input, output_channels, kernel_size=3, stride=1, scope="conv", activation=None):
     with tf.variable_scope(scope):
         activation_fn = None
@@ -21,8 +49,12 @@ def conv2d(batch_input, output_channels, kernel_size=3, stride=1, scope="conv", 
                            activation_fn=activation_fn)
 
 
+def lrelu_keras(input, alpha=0.2):
+    return keras.layers.LeakyReLU(alpha=alpha)(input)  # TODO: Pure tensorflow, Keras Not Allowed
+
+
 def lrelu(input, alpha=0.2):
-    return keras.layers.LeakyReLU(alpha=alpha)(input)
+    return tf.nn.leaky_relu(input, alpha=alpha)
 
 
 def average_pool(input, kernel_size, stride=2, scope="avg_pool"):
@@ -48,24 +80,27 @@ def decoder_block(input, skip_conn_input, output_channel, conv_kernel=3, up_scal
     with tf.variable_scope(scope):
         upsample = bilinear_upsampling(input, scale=up_scale)
 
-        block_input = tf.concat([input, skip_conn_input], 3)
-        _, _, _, inp_channels = input.get_shape()
+        block_input = tf.concat([upsample, skip_conn_input], 3)
+        _, _, _, upsample_channels = upsample.get_shape()
         _, _, _, skip_conn_channels = skip_conn_input.get_shape()
         _, _, _, total_channels = block_input.get_shape()
-        tf.debugging.assert_equal(inp_channels + skip_conn_channels, total_channels)  # TODO: Remove This Part
+        tf.debugging.assert_equal(upsample_channels + skip_conn_channels, total_channels)  # TODO: Remove This Part
+
+        net = conv2d(block_input, output_channel, kernel_size=conv_kernel)
+        net = lrelu(net, lrelu_alpha)
+        return net
 
 
-
-
-def UNet(inputs, output_channels, scope='unet', reuse=False, FLAGS=None):
+def UNet(inputs, output_channels, first_kernel=7, second_kernel=5, scope='unet', output_activation=None, reuse=False,
+         FLAGS=None):
     # Check the flag
     if FLAGS is None:
         raise ValueError('No FLAGS is provided for UNet')
 
     with tf.variable_scope(scope, reuse=reuse):
         with tf.variable_scope("encoder"):
-            econv1, epool1 = encoder_block(inputs, 32, conv_kernel=7, scope="en_conv1")
-            econv2, epool2 = encoder_block(epool1, 64, conv_kernel=5, scope="en_conv2")
+            econv1, epool1 = encoder_block(inputs, 32, conv_kernel=first_kernel, scope="en_conv1")
+            econv2, epool2 = encoder_block(epool1, 64, conv_kernel=second_kernel, scope="en_conv2")
             econv3, epool3 = encoder_block(epool2, 128, scope="en_conv3")
             econv4, epool4 = encoder_block(epool3, 256, scope="en_conv4")
             econv5, epool5 = encoder_block(epool4, 512, scope="en_conv5")
@@ -74,5 +109,19 @@ def UNet(inputs, output_channels, scope='unet', reuse=False, FLAGS=None):
                 econv6 = lrelu(econv6, alpha=0.1)
 
         with tf.variable_scope("decoder"):
+            net = decoder_block(econv6, econv5, 512, scope="dec_conv1")
+            net = decoder_block(net, econv4, 256, scope="dec_conv2")
+            net = decoder_block(net, econv3, 128, scope="dec_conv3")
+            net = decoder_block(net, econv2, 64, scope="dec_conv4")
+            net = decoder_block(net, econv1, 32, scope="dec_conv5")
 
-    return None
+        with tf.variable_scope("unet_output"):
+            net = conv2d(net, output_channels, scope="output")
+            if output_activation is not None:
+                if output_activation == "tanh":
+                    net = tf.nn.tanh(net)
+                elif output_activation == "lrelu":
+                    net = lrelu(net, alpha=0.1)
+                else:
+                    raise ValueError("only lrelu|tanh allowed")
+            return net, econv6
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import tensorflow.contrib.slim as slim
+
 # The operation used to print out the configuration
 def print_configuration_op(FLAGS):
     print('[Configurations]:')
@@ -15,3 +17,26 @@ def print_configuration_op(FLAGS):
             print('\t%s: %s' % (name, value))
 
     print('End of configuration')
+
+# VGG19 net
+def vgg_19(inputs, scope='vgg_19', reuse=False):
+    with tf.variable_scope(scope, 'vgg_19', [inputs], reuse=reuse) as sc:
+        end_points_collection = sc.name + '_end_points'
+        # Collect outputs for conv2d, fully_connected and max_pool2d.
+        with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+                            outputs_collections=end_points_collection):
+            net = slim.repeat(inputs, 2, slim.conv2d, 64, 3, scope='conv1', reuse=reuse)
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')
+            net = slim.repeat(net, 2, slim.conv2d, 128, 3, scope='conv2', reuse=reuse)
+            net = slim.max_pool2d(net, [2, 2], scope='pool2')
+            net = slim.repeat(net, 4, slim.conv2d, 256, 3, scope='conv3', reuse=reuse)
+            net = slim.max_pool2d(net, [2, 2], scope='pool3')
+            net = slim.repeat(net, 4, slim.conv2d, 512, 3, scope='conv4', reuse=reuse)
+            net = slim.max_pool2d(net, [2, 2], scope='pool4')
+            net = slim.repeat(net, 4, slim.conv2d, 512, 3, scope='conv5', reuse=reuse)
+            net = slim.max_pool2d(net, [2, 2], scope='pool5')
+            # Use conv2d instead of fully_connected layers.
+            # Convert end_points_collection into a end_point dict.
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+
+            return net, end_points
